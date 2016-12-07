0282ff2c43eada6c7755f0ddf6e4b9a181bae482
cdacunha71 <cdacunha71@gmail.com>
2016-12-07 22:27:18 +0100

    Revert "Credentials"
    This reverts commit 8a8a51dde50487303281c1a3cbeab3d304adc0d7.

--- testdbaccess.php
+++ testdbaccess.php
@@ -26,6 +26,6 @@ if (!$link) {
 
 echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
 echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
-echo "Credentials: " . $connectstr_dbusername . "/" . $connectstr_dbpassword . PHP_EOL;
+
 mysqli_close($link);
 ?>
